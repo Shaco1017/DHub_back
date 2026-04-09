8d2833c5d9ff4282298b9a75659ecbcaa141e948
D1ck_Head <fbyun1017@163.com>
2026-04-10 00:57:07 +0800

    后端初始化

---
+++ dhub_back/src/main/java/com/dhub/dhub_back/DhubBackApplication.java
@@ -0,0 +1,13 @@
+package com.dhub.dhub_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhubBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhubBackApplication.class, args);
+    }
+
+}
---
+++ dhub_back/src/test/java/com/dhub/dhub_back/DhubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhub.dhub_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhubBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
